69b2601cb293299721a754dd80b945d0ece35aa5
lihanqi <lihanqi@xa.reddatetech.com>
2025-12-04 18:04:56 +0800

    style: 主题色修改

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,5 @@ onHide(() => {
 });
 </script>
 <style lang="scss">
-// @import '/uni.scss';
+@import 'uview-plus/index.scss';
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
-import uviewPlus from 'uview-plus';
 
-import '@/styles/uview-plus.theme.scss';
-import 'uview-plus/index.scss';
+// import 'uview-plus/index.scss';
 import 'uno.css';
 
 export function createApp() {
   const app = createSSRApp(App);
-  app.use(uviewPlus);
   return {
     app
   };
