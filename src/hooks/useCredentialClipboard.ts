ab16be0c7b761e476301166858559271ac90a405
zhoudandan <zhoudandan@reddatetech.com>
2025-12-18 14:23:24 +0800

    fix: 是否导入-》一键粘贴

--- src/hooks/useCredentialClipboard.ts
+++ src/hooks/useCredentialClipboard.ts
@@ -19,7 +19,7 @@ export const useCredentialClipboard = (options: CredentialClipboardOptions = {})
   const {
     onMatch,
     title = '提示',
-    content = '检测到手机号档案凭证，是否导入？',
+    content = '检测到手机号档案凭证，是否一键粘贴？',
     confirmText = '导入',
     cancelText = '取消'
   } = options;
